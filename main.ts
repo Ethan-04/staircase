e56da5aa225da2e5dc29806550d23a89e2ba102c
Ethan-04 <58610690+Ethan-04@users.noreply.github.com>
2020-01-08 13:18:33 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let Juan = sprites.create(img`
+. . . . . . . f . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. d d d 8 8 8 8 8 8 8 d d d . . 
+. d d d 8 8 8 8 8 8 8 d d d . . 
+. d d d 8 8 8 8 8 8 8 d d d . . 
+. d d d 8 8 8 8 8 8 8 d d d . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f f f f f . . . . . 
+`, SpriteKind.Player)
+let Jeff = sprites.create(img`
+. . . . . . . . f . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . . d f d f d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . . . d d d . . . . . . 
+. . d d d 7 7 7 7 7 7 7 d d d . 
+. . d d d 7 7 7 7 7 7 7 d d d . 
+. . d d d 7 7 7 7 7 7 7 d d d . 
+. . d d d 7 7 7 7 7 7 7 d d d . 
+. . . . . 7 7 7 7 7 7 7 . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f f f f f f f . . . . 
+`, SpriteKind.Player)
+scene.setBackgroundColor(3)
+for (let index = 0; index < 9; index++) {
+    pause(100)
+    Juan.y += 4
+    Jeff.y += -6
+    pause(100)
+    Juan.x += 4
+    Jeff.x += -6
+}
